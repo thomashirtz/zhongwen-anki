5a165747a9c07be9cf96d99ad8554730b9cbc1dc
Thomas <hirtzt@gmail.com>
2022-10-20 20:00:51 +0800

    Update tatoeba.py

--- chinese_card_generator/tatoeba.py
+++ chinese_card_generator/tatoeba.py
@@ -1,5 +1,6 @@
 import requests
 from dataclasses import dataclass
+import hanzidentifier
 
 
 @dataclass
@@ -8,19 +9,24 @@ class Sentence:
     english: str
 
 
-# todo fix bug sometime sentence can be traditional, e.g. 防弹衣 => 湯姆穿著一件防彈衣，所以子彈沒把他殺死。
 def get_sentence(word: str) -> Sentence:
     query = f'https://tatoeba.org/en/api_v0/search?from=cmn&query="{word}"&to=eng'
-    print(query)
     response = requests.get(url=query)
+
     results = response.json()['results']
-    try:
-        return Sentence(
-            chinese=results[0]['text'],
-            english=results[0]['translations'][0][0]['text']
-        )
-    except IndexError:
-        return Sentence(chinese='', english='')
+    for result in results:
+        if hanzidentifier.is_simplified(result['text']):
+            try:
+                sentence = Sentence(
+                    chinese=result['text'],
+                    english=result['translations'][0][0]['text']
+                )
+                print(f"{word} - {sentence.chinese} - {sentence.english}")
+                return sentence
+            except IndexError:
+                pass
+
+    return Sentence(chinese='', english='')
 
 
 if __name__ == '__main__':
